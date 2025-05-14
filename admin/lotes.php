29a7e39ad4a738fc0ed81d9c115e24dd5b3c0c36
Ar1sAlan1s <Arisalanis468@gmail.com>
2025-05-13 23:25:32 -0600

    Carpeta lotes
    Agregue las carpeta Lotes para el crud

--- admin/lotes.php
+++ admin/lotes.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <title>LOTES</title>
 </head>
 <body>
-    <h2>Iniciar Sesión</h2>
+    <h2>LOTES</h2>
     <form method="POST">
         <input type="text" name="Id_Lote" placeholder="ID Lote" required><br>
         <input type="text" name="Tipo" placeholder="Tipo" required><br>
-        <input type="text" name="Fecha_Entrada" placeholder="Fexcha Entrada" required><br>
+        <input type="text" name="Fecha_Entrada" placeholder="Fecha Entrada" required><br>
         <input type="text" name="Fecha Caducidad" placeholder="Fecha Caducidad" required><br>
         <input type="text" name="Id_Producto" placeholder="Id Producto" required><br>
-        <button type="submit">Registrar Lote</button>
+        <button type="submit" url=>Registrar Lote</button>
     </form>
 </body>
 </html>
 
-<?php
-include("includes/db.php");
+<html>
+    <body>
+        pene
+    </body>
+</html>
 
-?>
