f4a84b0a780052f5e1f472ec0930f561b7e8686d
Ar1sAlan1s <Arisalanis468@gmail.com>
2025-05-13 23:48:47 -0600

    Eliminacion de carpeta chafa

--- admin/lotes.php
+++ admin/lotes.php
@@ -1,25 +1,13 @@
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
     <title>LOTES</title>
 </head>
 <body>
     <h2>LOTES</h2>
-    <form method="POST">
-        <input type="text" name="Id_Lote" placeholder="ID Lote" required><br>
-        <input type="text" name="Tipo" placeholder="Tipo" required><br>
-        <input type="text" name="Fecha_Entrada" placeholder="Fecha Entrada" required><br>
-        <input type="text" name="Fecha Caducidad" placeholder="Fecha Caducidad" required><br>
-        <input type="text" name="Id_Producto" placeholder="Id Producto" required><br>
-        <button type="submit" url=>Registrar Lote</button>
+    <form method="GET" action="../Lotes/add_Lote.php">
+        <button type="submit">Registrar Lote</button>
     </form>
 </body>
 </html>
-
-<html>
-    <body>
-        pene
-    </body>
-</html>
-
